d40401b4c38f335fadacc73b47bfecb19ee298e5
jake-the-cake <askjake331@gmail.com>
2022-11-05 23:57:04 -0400

    reset and color numbers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 "use strict";
+const PREFIX = '\x1b[';
+const SUFFIX = 'm';
+const colors = {
+    black: 0,
+    red: 1,
+    green: 2,
+    yellow: 3,
+    blue: 4,
+    magenta: 5,
+    cyan: 6,
+    white: 7
+};
 class TerminalLogger {
     constructor() {
         console.log('Log your terminal');
         this.colors = 'red';
+    }
+    reset() {
+        return `${PREFIX}0${SUFFIX}`;
     }
 }
 const x = new TerminalLogger();
---
+++ src/server.js
@@ -0,0 +1,6 @@
+"use strict";
+const Express = require('express');
+const APP = Express();
+APP.listen(6969, () => {
+    console.log('connected');
+});

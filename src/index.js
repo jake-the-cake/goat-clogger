6ac72daa263e1eb7082d1a70d36696dc219757cf
jake-the-cake <askjake331@gmail.com>
2022-11-07 22:50:26 -0500

    setattributes, color, usepreset, and a few

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 "use strict";
 const PREFIX = '\x1b[';
 const SUFFIX = 'm';
-const colors = {
+const groundsOnTerminal = {
+    fg: 3,
+    bg: 4
+};
+const colorsOnTerminal = {
     black: 0,
     red: 1,
     green: 2,
@@ -13,12 +17,53 @@ const colors = {
 };
 class TerminalLogger {
     constructor() {
-        console.log('Log your terminal');
-        this.colors = 'red';
+        this.colors = colorsOnTerminal;
+        this.grounds = groundsOnTerminal;
+        this.prefix = PREFIX;
+        this.suffix = SUFFIX;
+    }
+    setAttribute(value) {
+        return this.prefix + value + SUFFIX;
+    }
+    setColor({ fg, bg }) {
+        // Variables used to build format
+        const valueArray = [];
+        let value = '';
+        // Create array of color codes
+        if (fg) {
+            valueArray.push(String(this.grounds['fg']) +
+                String(this.colors[fg]));
+        }
+        if (bg) {
+            valueArray.push(String(this.grounds['bg']) +
+                String(this.colors[bg]));
+        }
+        // Go through array to string together formatted color codes
+        valueArray.forEach(val => {
+            value += this.setAttribute(Number(val));
+        });
+        return value;
     }
     reset() {
-        return `${PREFIX}0${SUFFIX}`;
+        return this.setAttribute(0);
+    }
+    usePreset(color, value) {
+        return this.setColor({
+            bg: color.bg,
+            fg: color.fg
+        }) + value + this.reset();
+    }
+    error(value) {
+        console.log(this.usePreset({
+            bg: 'red',
+            fg: 'white'
+        }, ` * ${value} * `));
+    }
+    warning(value) {
+        console.log(this.usePreset({
+            bg: 'yellow',
+            fg: 'black'
+        }, ` -- ${value} `));
     }
 }
-const x = new TerminalLogger();
-console.log(x.colors);
+module.exports = TerminalLogger;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 "use strict";
 const Express = require('express');
+const TLog = require('./index');
 const APP = Express();
+APP.get('/', (req, res) => {
+    const tlg = new TLog();
+    tlg.error('Preset: Error ==> tlg.error( value )');
+    tlg.warning('Preset: Warning ==> tlg.warning( value )');
+    res.send('check your terminal');
+});
 APP.listen(6969, () => {
     console.log('connected');
 });
